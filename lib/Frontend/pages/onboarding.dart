5d2ba68e202e78447d163413e1b38a0096c8779d
rjrishav09 <crazyrishavhacker@gmail.com>
2023-09-14 03:57:24 +0530

    onboarding page change

--- lib/Frontend/pages/onboarding.dart
+++ lib/Frontend/pages/onboarding.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 import 'package:sih_2023/Frontend/pages/auth/login.dart';
 import 'package:sih_2023/Frontend/pages/auth/signup.dart';
 import 'package:sih_2023/Frontend/pages/homepage.dart';
+
+import '../../widgets/build_images.dart';
 
 class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
@@ -26,47 +29,85 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
       SystemUiOverlayStyle.dark.copyWith(statusBarColor: Colors.transparent),
     );
     return Scaffold(
-      body: SafeArea(
-        child: PageView(
-          children: [
-            //change onboarding screen...
-            Container(
-              color: Colors.red,
-              child: const Center(
-                child: Text("First"),
-              ),
-            ),
-            Container(
-              color: Colors.blue,
-              child: const Center(
-                child: Text("Second"),
-              ),
-            ),
-            Container(
-              color: Colors.green,
-              child: Center(
-                child: Column(
-                  children: [
-                    const Text("Third"),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.of(context).pushReplacement(
-                          MaterialPageRoute(
-                            builder: (context) => const Login(),
-                          ),
-                        );
-                      },
-                      child: const Text(
-                        "Get Started",
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            )
-          ],
-        ),
+      body: IntroductionScreen(
+        pages: [
+          PageViewModel(
+            title: 'Aristotle',
+            body:
+            'The law is reason, free from passion.',
+            image: const BuildImages(image: 'assets/images/image1.jpg'),
+            decoration: getPageDecoration(),
+          ),
+          PageViewModel(
+            title: 'John Locke',
+            body:
+            'Where there is no law, there is no freedom.',
+            image: const BuildImages(image: 'assets/images/image2.jpg'),
+            decoration: getPageDecoration(),
+          ),
+          PageViewModel(
+            title: 'Cicero',
+            body:
+            'The good of the people is the greatest law.',
+            image: const BuildImages(image: 'assets/images/image3.jpg'),
+          )
+        ],
+        next: const Icon(Icons.arrow_forward),
+        done: const Text('Done', style: TextStyle(fontWeight: FontWeight.bold)),
+        onDone: () => goToHome(context),
+        showSkipButton: true,
+        skip: const Text('Skip'),
+        // onSkip: () => print('skip'),
+        // showBackButton: true,
+        // back: const Icon(Icons.arrow_back),
+        dotsDecorator: getDotDecoration(),
+        nextFlex: 0,
+        skipOrBackFlex: 0,
+        animationDuration: 500,
+        isProgressTap: true,
+        isProgress: true,
+        // freeze: false,
+        onChange: (index) => print(index),
       ),
     );
   }
+
+  PageDecoration getPageDecoration() {
+    return const PageDecoration(
+      titleTextStyle: TextStyle(fontSize: 28, fontWeight: FontWeight.w600),
+      bodyTextStyle: TextStyle(fontSize: 20),
+      imagePadding: EdgeInsets.all(24),
+      titlePadding: EdgeInsets.zero,
+      bodyPadding: EdgeInsets.all(20),
+      pageColor: Colors.white,
+    );
+  }
+
+  DotsDecorator getDotDecoration() {
+    return DotsDecorator(
+        color: const Color(0xFFBDBDBD),
+        size: const Size(10, 10),
+        activeSize: const Size(22, 10),
+        activeColor: Colors.orange,
+        activeShape:
+        RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)));
+  }
+
+  void goToHome(context) {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => const Login()),
+    );
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
